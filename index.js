9060363cd155688aeb79020bdff5b7621e0d19ff
JEmilioC <28cllje@gmail.com>
2017-10-25 11:23:24 -0500

    CommitFunctional

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
 // Read the file and print its contents.
-var app= require('express')();
-var http= require('http').Server(app);
-var Cloudant=require('cloudant');
-var me = '';
-// var username = process.env.cloudant_username || "nodejs";
-// var password = process.env.cloudant_password;
-// var cloudant = Cloudant("https://56c7d887-694a-47f2-8bda-0f9323cc7a0e-bluemix:1a932789536cbff21413a13691dbf6e6d03f34a51c6ac7ecdb4334b45cfa62c3@56c7d887-694a-47f2-8bda-0f9323cc7a0e-bluemix.cloudant.com");
- 
-var fs = require('fs')
-, filename = "./EMEA.csv";
-fs.readFile(filename, 'utf8', function(err, data) {
-    if (err) throw err;
-    console.log('OK: ' + filename);
-    const lines = data.split('\n').map(line => {
-        return line.replace('\r', '').split(',')
-    });
-    const cloudantObjects = lines.map(l => {
-        return {
-            region: l[0],
-            countryNumber: l[1],
-            countryName: l[2],
-            userID: l[3]
-        }
-    });
-    app.post('/', function(req,res){
-        var db=cloudant.db.use('empldb')
-        cloudantObjects.forEach(cloudObj => {
-            db.insert(cloudObj,(err,doc)=>{
-                if(err){res.status(400).end()}      
-                else{res.json(cloudObj)}
-            });
+const app= require('express')();
+const http= require('http').Server(app);
+const Cloudant=require('cloudant');
+const fs = require('fs')
+const filename = "./EMEA.csv";
+const account = "7f7927e0-886e-48f7-bc91-680d9a7d81b4-bluemix";
+const password = "c1be51bb8656cbaa14191468a4e48da23e15da01c7aa565136ce1ca098e436e2";
+const cloudant = Cloudant({account, password}, (err, cloudant) => {
+  if (err) {
+    console.log('db error')
+  } else {
+    fs.readFile(filename, 'utf8', function(err, data) {
+        if (err) throw err;
+        console.log('OK: ' + filename);
+        const lines = data.split('\n').map(line => {
+            return line.replace('\r', '').split(',')
         });
+        const cloudantObjects = lines.map(l => {
+            return {
+                region: l[0],
+                countryNumber: l[1],
+                countryName: l[2],
+                userID: l[3]
+            }
+        });
+        const db=cloudant.db.use('test')
+        db.bulk({docs: cloudantObjects}, (err, docs) => {
+          if (err) {
+            console.log(err)
+          } else {
+            console.log('docs inserted')
+          }
+        })
     });
-});
+  }
+})

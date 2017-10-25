f76c4c1b94ccbc24d8d831102bca92fd8f3a587d
JEmilioC <28cllje@gmail.com>
2017-10-25 10:22:59 -0500

    Commit#2

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var app= require('express')();
 var http= require('http').Server(app);
 var Cloudant=require('cloudant');
 var me = '';
-var username = process.env.cloudant_username || "nodejs";
-var password = process.env.cloudant_password;
-var cloudant = Cloudant("https://56c7d887-694a-47f2-8bda-0f9323cc7a0e-bluemix:1a932789536cbff21413a13691dbf6e6d03f34a51c6ac7ecdb4334b45cfa62c3@56c7d887-694a-47f2-8bda-0f9323cc7a0e-bluemix.cloudant.com");
+// var username = process.env.cloudant_username || "nodejs";
+// var password = process.env.cloudant_password;
+// var cloudant = Cloudant("https://56c7d887-694a-47f2-8bda-0f9323cc7a0e-bluemix:1a932789536cbff21413a13691dbf6e6d03f34a51c6ac7ecdb4334b45cfa62c3@56c7d887-694a-47f2-8bda-0f9323cc7a0e-bluemix.cloudant.com");
  
 var fs = require('fs')
 , filename = "./EMEA.csv";
@@ -24,16 +24,12 @@ fs.readFile(filename, 'utf8', function(err, data) {
         }
     });
     app.post('/', function(req,res){
-        // res.sendFile(__dirname+'/index.html');
-    
-    var db=cloudant.db.use('empldb')
-    cloudantObjects.forEach(cloudObj => {
-        db.insert(cloudObj,(err,doc)=>{
-            if(err){
-                res.status(400).end()
-            }
-            else{res.json(cloudObj)}
+        var db=cloudant.db.use('empldb')
+        cloudantObjects.forEach(cloudObj => {
+            db.insert(cloudObj,(err,doc)=>{
+                if(err){res.status(400).end()}      
+                else{res.json(cloudObj)}
+            });
         });
-    })
-})
+    });
 });
